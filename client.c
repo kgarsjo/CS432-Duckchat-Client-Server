d43ab1b57b55b59b672b6d42cc3c09ba28e35d20
kgarsjo <kgarsjo@kgarsjo-VirtualBox.(none)>
2012-10-31 16:33:14 -0700

    Fixing select

--- client.c
+++ client.c
@@ -67,13 +67,14 @@ int main(int argc, char** argv) {
 
 	int parseStatus= true;
 	fd_set readfds;
-	FD_ZERO(&readfds);
-	FD_SET(0, &readfds);
-	FD_SET(sockfd, &readfds);
 	prompt();
 
 	do {
+		FD_ZERO(&readfds);
+		FD_SET(0, &readfds);
+		FD_SET(sockfd, &readfds);
 		select(sockfd+1, &readfds, NULL, NULL, NULL);
+		
 		if (FD_ISSET(0, &readfds)) {
 			char *input= new_inputString();
 			if (input != NULL) {
